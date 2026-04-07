12781a2ea78555c1f49d35c4ee89c32065d1236d
roxannication-arch <roxannication@gmail.com>
2026-04-07 13:56:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -314,4 +314,6 @@ def api_generate():
 
 if __name__ == '__main__':
     print("🚀 Career Radar: http://localhost:5000")
-    app.run(debug=False, port=5000)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port, debug=False)
